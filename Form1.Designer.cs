792ed1883315b86a62043bc15928bdeacb5a1591
tatevik.hakobyann@gmail.com <tatevik.hakobyann@gmail.com>
2021-11-09 16:47:02 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace EventApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.namelbl = new System.Windows.Forms.Label();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.lastnameLbl = new System.Windows.Forms.Label();
+            this.lastnameBox = new System.Windows.Forms.TextBox();
+            this.nameValidLbl = new System.Windows.Forms.Label();
+            this.lastnameValidLbl = new System.Windows.Forms.Label();
+            this.studentGridView = new System.Windows.Forms.DataGridView();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.studentGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(226, 154);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(68, 28);
+            this.saveBtn.TabIndex = 0;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // namelbl
+            // 
+            this.namelbl.AutoSize = true;
+            this.namelbl.Location = new System.Drawing.Point(48, 73);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(45, 17);
+            this.namelbl.TabIndex = 1;
+            this.namelbl.Text = "Name";
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(138, 70);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(119, 22);
+            this.namebox.TabIndex = 2;
+            this.namebox.TextChanged += new System.EventHandler(this.Name_TextChanged);
+            this.namebox.GotFocus += new System.EventHandler(this.Name_TextChanged);
+            // 
+            // lastnameLbl
+            // 
+            this.lastnameLbl.AutoSize = true;
+            this.lastnameLbl.Location = new System.Drawing.Point(48, 117);
+            this.lastnameLbl.Name = "lastnameLbl";
+            this.lastnameLbl.Size = new System.Drawing.Size(70, 17);
+            this.lastnameLbl.TabIndex = 3;
+            this.lastnameLbl.Text = "Lastname";
+            // 
+            // lastnameBox
+            // 
+            this.lastnameBox.Location = new System.Drawing.Point(138, 114);
+            this.lastnameBox.Name = "lastnameBox";
+            this.lastnameBox.Size = new System.Drawing.Size(119, 22);
+            this.lastnameBox.TabIndex = 4;
+            this.lastnameBox.TextChanged += new System.EventHandler(this.lastname_TextChanged);
+            this.lastnameBox.GotFocus += new System.EventHandler(this.lastname_TextChanged);
+            // 
+            // nameValidLbl
+            // 
+            this.nameValidLbl.AutoSize = true;
+            this.nameValidLbl.Location = new System.Drawing.Point(135, 94);
+            this.nameValidLbl.Name = "nameValidLbl";
+            this.nameValidLbl.Size = new System.Drawing.Size(0, 17);
+            this.nameValidLbl.TabIndex = 5;
+            // 
+            // lastnameValidLbl
+            // 
+            this.lastnameValidLbl.AutoSize = true;
+            this.lastnameValidLbl.Location = new System.Drawing.Point(135, 139);
+            this.lastnameValidLbl.Name = "lastnameValidLbl";
+            this.lastnameValidLbl.Size = new System.Drawing.Size(0, 17);
+            this.lastnameValidLbl.TabIndex = 6;
+            // 
+            // studentGridView
+            // 
+            this.studentGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentGridView.Location = new System.Drawing.Point(5, 228);
+            this.studentGridView.Name = "studentGridView";
+            this.studentGridView.RowHeadersWidth = 51;
+            this.studentGridView.RowTemplate.Height = 24;
+            this.studentGridView.Size = new System.Drawing.Size(346, 218);
+            this.studentGridView.TabIndex = 7;
+            this.studentGridView.SelectionChanged += new System.EventHandler(this.StudentGridView_SelectionChanged);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.AutoEllipsis = true;
+            this.updateBtn.Location = new System.Drawing.Point(141, 154);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(68, 28);
+            this.updateBtn.TabIndex = 8;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(50, 154);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(68, 28);
+            this.deleteBtn.TabIndex = 9;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 448);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.studentGridView);
+            this.Controls.Add(this.lastnameValidLbl);
+            this.Controls.Add(this.nameValidLbl);
+            this.Controls.Add(this.lastnameBox);
+            this.Controls.Add(this.lastnameLbl);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.namelbl);
+            this.Controls.Add(this.saveBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.studentGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Label namelbl;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.Label lastnameLbl;
+        private System.Windows.Forms.TextBox lastnameBox;
+        private System.Windows.Forms.Label nameValidLbl;
+        private System.Windows.Forms.Label lastnameValidLbl;
+        private System.Windows.Forms.DataGridView studentGridView;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventApp
+{
+    public partial class Form1 : Form
+    {
+        private const string requiredMessage = "Required";
+        private readonly  StudentService _studentService;
+        public Form1()
+        {
+            this._studentService = new StudentService();
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(namebox.Text) || string.IsNullOrEmpty(lastnameBox.Text))
+            {
+                MessageBox.Show("Please enter student's name and last name");
+            }
+            else
+            {
+                _studentService.Add(new Student { Name = namebox.Text, LastName = lastnameBox.Text });
+                List<Student> students = _studentService.GetAll();
+                UpdateDataSource(studentGridView, students);
+            }
+        }
+        private void UpdateDataSource(DataGridView dataGridView, List<Student> students)
+        {
+            studentGridView.DataSource = null;
+            studentGridView.Update();
+            studentGridView.DataSource = students;
+            studentGridView.Update();
+        }
+        private void Name_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(namebox.Text))
+            {
+                nameValidLbl.Text = requiredMessage;
+                nameValidLbl.ForeColor = Color.Red;
+            }
+            else
+            {
+                nameValidLbl.Text = string.Empty;
+            }
+        }
+
+        private void lastname_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(lastnameBox.Text))
+            {
+                lastnameValidLbl.Text = requiredMessage;
+                lastnameValidLbl.ForeColor = Color.Red;
+            }
+            else
+            {
+                lastnameValidLbl.Text = string.Empty;
+            }
+        }
+        private void StudentGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            namebox.Text = studentGridView.CurrentRow.Cells[1].Value.ToString();
+            lastnameBox.Text = studentGridView.CurrentRow.Cells[2].Value.ToString();
+        }
+       
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            if (namebox.Text!="" && lastnameBox.Text!="")
+            {
+                studentGridView.CurrentRow.Cells[1].Value=  namebox.Text;
+                studentGridView.CurrentRow.Cells[2].Value = lastnameBox.Text;
+                MessageBox.Show("Successfully updated");
+            }
+            else
+            {
+                MessageBox.Show("Has not updated");
+            }
+            
+
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (namebox.Text != "" && lastnameBox.Text != "")
+            { 
+                studentGridView.CurrentRow.Cells[0].Value = null;
+                studentGridView.CurrentRow.Cells[1].Value = null;
+                studentGridView.CurrentRow.Cells[2].Value = null;
+                MessageBox.Show("Successfully deleted");
+            }
+            else
+            {
+                MessageBox.Show("Has not deleted");
+            }
+        }
+    }
+    public class Student
+    {
+        public Student()
+        {
+            Id =Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+       
+    }
+    public class StudentService
+    {
+        public static List<Student> _students = new List<Student>();
+        public void Add(Student student)
+        {
+            _students.Add(student);
+        }
+        public List<Student> GetAll()
+        {
+            return _students;
+        }
+       
+    }
+}
